0008320a6b62b5122c4200bcb9adce0c82d825ab
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-09-18 21:52:15 +0300

    fix profile.js

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Profile from './components/Profile';
+import user from './components/user';
+export  const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
