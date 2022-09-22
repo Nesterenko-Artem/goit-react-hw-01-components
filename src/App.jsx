2bc0fca29a817dff98cae86e3bcae175657e483b
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-09-22 23:15:00 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Profile from './components/Profile';
-import user from './components/user';
+import Profile from 'components/Profile';
+import user from 'user';
 export  const App = () => {
   return (
     <div
@@ -12,6 +12,16 @@ export  const App = () => {
         color: '#010101',
       }}
     >
+      {/* {[1,2,3,4,5].map(el => <div>{el}</div>)} */}
+      {/* {Profile.map(profil =>
+       <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+        )} */}
       <Profile
         username={user.username}
         tag={user.tag}
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,43 @@
+import PropTypes from 'prop-types';
 
+export default function Profile({
+  username,
+  avatar,
+  tag,
+  location,
+  stats
+}) {
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt={tag} class="avatar image " />
+        <p class="name">{username}</p>
+        <p class="tag">@{tag}</p>
+        <p class="location">{location}</p>
+      </div>
 
-export default function Profile({username,avatar,tag,location,stats}) {
-   return (
-     <div class="profile">
-       <div class="description">
-         <img src={avatar} alt={tag} class="avatar" />
-         <p class="name">{username}</p>
-         <p class="tag">@{tag}</p>
-         <p class="location">{location}</p>
-       </div>
-
-       <ul class="stats">
-         <li>
-           <span class="label">Followers</span>
-           <span class="quantity">{stats.followers}</span>
-         </li>
-         <li>
-           <span class="label">Views</span>
-           <span class="quantity">{stats.views}</span>
-         </li>
-         <li>
-           <span class="label">Likes</span>
-           <span class="quantity">{stats.likes}</span>
-         </li>
-       </ul>
-     </div>
-   );
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+Profile.prototype = {
+  username: PropTypes.string,
+  avatar: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  stats: PropTypes.object,
+};
