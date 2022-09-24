2cc8e9c37ab29e741b2071772c04e47dab2b0b34
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-09-24 14:03:24 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,17 @@
-import Profile from 'components/Profile';
+import {Profile} from './components/Profile';
 import user from 'user';
-export  const App = () => {
+
+export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      {/* {[1,2,3,4,5].map(el => <div>{el}</div>)} */}
-      {/* {Profile.map(profil =>
-       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-        )} */}
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-    </div>
+    <> 
+     <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    </>
+   
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,43 +1,51 @@
 import PropTypes from 'prop-types';
+import {
+  Wraper,
+  Avatar,
+  Card,
+  Text,
+  List,
+  Label,
+  Quantity,
+  StatsItem,
+} from './Profile.styled';
 
-export default function Profile({
-  username,
-  avatar,
-  tag,
-  location,
-  stats
-}) {
+export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
-    <div class="profile">
-      <div class="description">
-        <img src={avatar} alt={tag} class="avatar image " />
-        <p class="name">{username}</p>
-        <p class="tag">@{tag}</p>
-        <p class="location">{location}</p>
-      </div>
+    <Wraper>
+      <Card>
+        <Avatar src={avatar} alt={tag} />
+        <Text className="name">{username}</Text>
+        <Text className="tag">@{tag}</Text>
+        <Text className="location">{location}</Text>
+      </Card>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{stats.likes}</span>
-        </li>
-      </ul>
-    </div>
+      <List>
+        <StatsItem>
+          <Label className="label">Followers</Label>
+          <Quantity className="quantity">{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label className="label">Views</Label>
+          <Quantity className="quantity">{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label className="label">Likes</Label>
+          <Quantity className="quantity">{stats.likes}</Quantity>
+        </StatsItem>
+      </List>
+    </Wraper>
   );
-}
+};
 
-Profile.prototype = {
-  username: PropTypes.string,
-  avatar: PropTypes.string,
-  tag: PropTypes.string,
-  location: PropTypes.string,
-  stats: PropTypes.object,
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
 };
