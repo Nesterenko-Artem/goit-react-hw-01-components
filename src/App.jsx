1d5e546cd53bd791e6ef334c5096c58aac513d8f
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-09-25 10:27:30 +0300

    fix task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 // import {Profile} from './components/Profile/Profile';
-import { StatItem } from './components/Statistics/StatItem';
+import { StatList } from './components/Statistics/StatList';
 
 // import user from './path/user.json';
-import stats from './path/stats';
-console.log(stats)
+import stats from './path/stats.json';
+
+
 export const App = () => {
   return (
     <>
-      <StatItem 
-      label={stats.label} 
-      percentage={stats.percentage} 
-      />
-{/* 
-      <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    /> */}
+      <h1>Task 1</h1>
+      {/* <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      /> */}
+      <h1>Task 2</h1>
+
+      <StatList title="Upload stats" stats={stats} />
     </>
   );
 };
---
+++ src/components/Statistics/StatList.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+export const StatList = ({ stats, title }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+      <ul>
+        {stats.map(stat => (
+          <li className="item" key={stat.id}>
+            <span className="label">{stat.label}</span>
+            <span className="percentage">{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+StatList.propTypes = {
+  title: PropTypes.string.isRequired,
+
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
